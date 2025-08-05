293837a937942e50d29e6e3330e74d4d428619c7
Caio Guilherme <caiobalbino@tce.mt.gov.br>
2025-08-05 12:32:23 -0400

    Alterando tema

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+
 
 import { routes } from './app.routes';
+import MyPreset from './mypreset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,7 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: Aura
+                preset: MyPreset
             }
         }),
   ]
